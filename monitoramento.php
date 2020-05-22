a7b35c45522911c0d2f85cb7d5b280c6bb1fc2f4
Arley Souto <32252053+mercurio236@users.noreply.github.com>
2020-05-22 16:57:34 -0300

    ajuste na tela de cadastro feitas

--- monitoramento.php
+++ monitoramento.php
@@ -9,64 +9,9 @@
 </head>
 
 <body>
-    <div class="container" style="margin-top: 8%;">
-        <!--foi colocado um style na tag para afastar as tabelas do topo-->
-        <div class="row">
-
-            <div class="col">
-                <!--Tabela de entrada, aqui defeine a largura do Grid em 50% -->
-                <h2 style="text-align:center;">Entrada</h2>
-                <!--Titulo da tabela-->
-                <table class="table table-striped table-bordered">
-                    <!-- aqui inicia a tabela de saída-->
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Data</th>
-                            <th scope="col">Hora</th>
-                            <th scope="col">Setor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    
 
 
-            <div class="col">
-                <!--Tabela de saída, aqui define a largura do Grid em 50% -->
-                <h2 style="text-align:center;">Saída</h2>
-                <!--titulo da tabela-->
-                <table class="table table-striped table-bordered">
-                    <!-- aqui inicia a tabela de saída-->
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">Data</th>
-                            <th scope="col">Hora</th>
-                            <th scope="col">Setor</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
 </body>
 
 </html>
@@ -82,18 +27,76 @@ $pesquisa_nome = mysqli_query($conn, $resultado_pes);
   $res = $conn -> query($resultado_pes);
   $qtd = $res -> num_rows;
 
+  print "
+  <div class='container' style='margin-top: 8%;'>
+        <!--foi colocado um style na tag para afastar as tabelas do topo-->
+        <div class='row'>
+
+            <div class='col-6'>
+               <!--Tabela de entrada, aqui defeine a largura do Grid em 50% -->
+                <h2 style='text-align:center;'>Entrada</h2>
+                <!--Titulo da tabela-->
+                <table class='table table-striped table-bordered'>
+                    <!-- aqui inicia a tabela de saída-->
+                    <thead>
+                        <tr>
+                            <th scope='col'>#</th>
+                            <th scope='col'>Nome</th>
+                            <th scope='col'>Data</th>
+                            <th scope='col'>Hora</th>
+                            <th scope='col'>Setor</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+  
+  ";
   while($rows_pesquisa = mysqli_fetch_array($pesquisa_nome)){
+    
     if($qtd > 0){
       while($row = $res -> fetch_assoc()){
-        print "foi encontrado " .$row["nomeUsuario"]. $row["setor"];
-        print "<tr>
-                            <th scope='row'>1</th>
-                            <td>Mark</td>
-                            <td>Otto</td>
-                            <td>@mdo</td>
-                        </tr>"
+        //print "foi encontrado " .$row["nomeUsuario"]. $row["setor"];
+        print "
+        <div class='container' style='margin-top: -1.5%;'>
+        <div class='row'>
+        <div class='col-6'>
+        <table class='table table-striped table-bordered'>
+        <tbody>
+            <tr>
+                <th scope='row'>1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>setor</td>
+            </tr>
+        </tbody>
+    </table>
+    
+    </div>
+    </div>
+    </div>";//fim da lista
+
+   /* print "
+    <div class='container' style='margin-top: -1.5%;'>
+        <div class='row'>
+        <div class='col-6'>
+        <table class='table table-striped table-bordered'>
+        <tbody>
+            <tr>
+                <th scope='row'>1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                <td>setor</td>
+            </tr>
+        </tbody>
+    </table>
+    
+    </div>
+    </div>
+    </div>";*/
       }
-      print "$qtd<p> usuários cadastrados</p>";
+     
     }else{
       print "<p>Resultado não entrado</p>";
     }
