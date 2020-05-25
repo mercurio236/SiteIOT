84279a47e4639284a91c76d60b313b5f2c616205
Arley Souto <32252053+mercurio236@users.noreply.github.com>
2020-05-24 21:34:34 -0300

    Codigo comentado

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
   	<a class="nav-link" style="color:white;" href="index.php?page=monitoramento">Monitoramento</a>
 </nav>
 
-<!--Alternação entre as paginas -->
+<!--Transição entre as paginas -->
 <div class="container">
 			<div class="row">
 				<div class="col-lg-12">
