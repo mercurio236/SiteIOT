f011498ad7aa6c9cd6536264f5d711f767c7c576
Arley Souto <32252053+mercurio236@users.noreply.github.com>
2020-05-24 21:41:41 -0300

    Codigo todo comentado

--- config.php
+++ config.php
@@ -1,4 +1,14 @@
 <?php
+/* 
+=====================================================================
+* Este arquivo é o mais importante, pois ele fica todas as informações de credenciamento do banco
+* host = local
+* user = login
+* pass = senha
+* base = nome do banco
+* em conn ele testa a conexão com o banco
+=======================================================================
+*/
 	$host = "localhost";
 	$user = "root";
 	$pass = "";
--- info.php
+++ info.php
@@ -1,16 +1,16 @@
 <?php
-$nome = @$_REQUEST["nomeUsuario"];
-$setor = $_REQUEST["setorUsuario"];
+$nome = @$_REQUEST["nomeUsuario"];//varivel para armazenar o campo para cadastrar no banco
+$setor = $_REQUEST["setorUsuario"];//variavel para cadastrar o que está no campo em html
 
-        $sql = "insert into cadastro (nomeUsuario, setor) values ('{$nome}', '{$setor}')";
-        $result = $conn ->query($sql);
+        $sql = "insert into cadastro (nomeUsuario, setor) values ('{$nome}', '{$setor}')";//insere no banco
+        $result = $conn ->query($sql);//faz a conexão com o banco
     
-        if($result==true){
+        if($result==true){//mensagem de cadastro bem sucessedido no banco
             print "<div class='alert alert-success'>Cadastrou com sucesso!</div>";
             echo "<meta http-equiv='refresh' content='1;index.php?page=cadastro'>";//refresh para não ficar na mesma pagina
         }
         
-        else{
+        else{//mensagem de que não possivel cadastrar no banco
             print "<div class='alert alert-danger'>Não foi possível cadastrar</div>"; 
             echo "<meta http-equiv='refresh' content='1;index.php?page=cadastro'>";
         }
