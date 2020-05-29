ad3215c5a74e77d395843fe355612ea772c43940
Arley Souto <32252053+mercurio236@users.noreply.github.com>
2020-05-29 18:11:01 -0300

    Foi colocado novas paginas

--- index.php
+++ index.php
@@ -1,25 +1,31 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Monitoramento</title>
 </head>
-<body>
+
 <body>
 
-<!-- Imagem e texto do menu -->
-<nav class="navbar justify-content-center navbar-dark bg-dark">
-  	<a class="nav-link" style="color:white;" href="index.php?page=cadastro">Cadastro</a>
-  	<a class="nav-link" style="color:white;" href="index.php?page=monitoramento">Monitoramento</a>
-</nav>
+    <body>
 
-<!--Transição entre as paginas -->
-<div class="container">
-			<div class="row">
-				<div class="col-lg-12">
-					<?php
+        <!-- Imagem e texto do menu -->
+        <nav class="navbar justify-content-center navbar-dark bg-dark">
+            <a class="nav-link" style="color: white;" href="index.php?page=home">Home</a>
+            <a class="nav-link" style="color:white;" href="index.php?page=cadastro">Cadastro</a>
+            <a class="nav-link" style="color:white;" href="index.php?page=monitoramento">Monitoramento</a>
+            <a class="nav-link" style="color:white;" href="index.php?page=login">Login</a>
+
+        </nav>
+
+        <!--Transição entre as paginas -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <?php
 					
 						include("config.php");//responsavel por fazer a conexão no banco
 						
@@ -35,10 +41,18 @@
 							case "info":
 								include("info.php");
 							break;
+
+							case "login":
+								include("login.php");
+							break;
+
+							default:
+								include("home.php");
 						}
 					?>
-				</div>
-			</div>
-		</div> 
-</body>
+                </div>
+            </div>
+        </div>
+    </body>
+
 </html>
