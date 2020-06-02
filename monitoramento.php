39ad2e5e1f2ef5d6e3d28d666e1bbed3e97d029d
Arley Souto Aguiar <macx15@hotmail.com>
2020-06-02 17:49:55 -0300

    Reponsividade para mobile e Desktop
    Feito o auste de responsividade, funcionamento tanto mobile quanto Destop

--- monitoramento.php
+++ monitoramento.php
@@ -68,66 +68,66 @@
 	if(($qtdEnt > 0 && strcmp($tipoMov,"entrada")) == 0 ||
 	   ($qtdSai > 0 && strcmp($tipoMov,"saida") == 0)){
 	//estilo de exibição de pesquisa
-	print "
-	<div class='container' style='margin-top: 8%;'>
-		<!--foi colocado um style na tag para afastar as tabelas do topo-->
-        <div class='row'>
-
-            <div class='col'>
-               <!--Tabela de entrada, aqui defeine a largura do Grid em 50% -->";
+	
 				if(strcmp($tipoMov,"entrada") == 0){
 					print"<h2 style='text-align:center;'>Entradas</h2>";
 				}else{
 					print"<h2 style='text-align:center;'>Saidas</h2>";
 				}
-				print"
+				//pesquisa do banco com resultado de quantidade
+	
+		
+							//print "foi encontrado " .$row["nomeUsuario"]. $row["setor"];
+			 print"
                 <!--Titulo da tabela-->
-                <table class='table table-striped table-bordered'>
+                <table class='table table-sm'>
                     <!-- aqui inicia a tabela de saída-->
                     <thead>
                         <tr>
-                            <th scope='col'  style='width:3.7%;'>$qtd</th>
-                            <th scope='col'  style='width:34.1%;'>Nome</th>
-                            <th scope='col'  style='width:11.4%;'>Data</th>
-                            <th scope='col'  style='width:11.3%;'>Hora</th>
-                            <th scope='col'  style='width:28.4%;'>Local</th>
+                            <th scope='col' >$qtd</th>
+                            <th scope='col' >Nome</th>
+                            <th scope='col' >Data</th>
+                            <th scope='col' >Hora</th>
+                            <th scope='col' >Local</th>
                             <th scope='col'>Card</th>
                         </tr>
-                    </thead>
-                </table>
-            </div>
-  
-	";
-	print "
-		<div class='container' style='margin-top: -1.5%;'>
-			<div class='row'>
-			<div class='col-12'>
-			<table class='table table-striped table-bordered'>
-			<tbody>
-				<tr>";
-	//pesquisa do banco com resultado de quantidade
-    if (strcmp($tipoMov,"entrada") == 0){
+                    </thead>";
+			
+	if (strcmp($tipoMov,"entrada") == 0){
 		while($rows_pesquisa = mysqli_fetch_array($pesquisa_nome_ent)){
 			while($row = $resEnt -> fetch_assoc()){
-				//print "foi encontrado " .$row["nomeUsuario"]. $row["setor"];
-				print " <th scope='row' style='width:3%;'>$row[id_cadastro]</th>
-						<td style='width:30%;'>$row[no_usuario]</td>
-						<td style='width:10%;'>$row[dt_ocorrencia]</td>
-						<td style='width:10%;'>$row[hr_ocorrencia]</td>
-						<td style='width:25%;'>$row[no_localizacao]</td>
-						<td style='width:10%;'>$row[cd_cartao]</td>";
-			}
-		}
-	}else{
+		
+			print"<tbody>
+				<tr>
+					<th scope='row'>$row[id_cadastro]</th>
+						<td>$row[no_usuario]</td>
+						<td>$row[dt_ocorrencia]</td>
+						<td>$row[hr_ocorrencia]</td>
+						<td>$row[no_localizacao]</td>
+						<td>$row[cd_cartao]</td>
+				</tr>
+			</tbody>
+			";
+		
+					
+				
+			}//fim do while 2
+		}//fim do while 1
+	}//fim do if
+	else{
 		while($rows_pesquisa = mysqli_fetch_array($pesquisa_nome_sai)){
 			while($row = $resSai -> fetch_assoc()){
-				//print "foi encontrado " .$row["nomeUsuario"]. $row["setor"];
-				print " <th scope='row' style='width:3%;'>$row[id_cadastro]</th>
-						<td style='width:30%;'>$row[no_usuario]</td>
-						<td style='width:10%;'>$row[dt_ocorrencia]</td>
-						<td style='width:10%;'>$row[hr_ocorrencia]</td>
-						<td style='width:25%;'>$row[no_localizacao]</td>
-						<td style='width:10%;'>$row[cd_cartao]</td>";
+				print"<tbody>
+				<tr>
+					<th scope='row'>$row[id_cadastro]</th>
+						<td>$row[no_usuario]</td>
+						<td>$row[dt_ocorrencia]</td>
+						<td>$row[hr_ocorrencia]</td>
+						<td>$row[no_localizacao]</td>
+						<td>$row[cd_cartao]</td>
+				</tr>
+			</tbody>
+			";
 			}
 		}
 	}
