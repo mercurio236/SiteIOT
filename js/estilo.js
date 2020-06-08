01bd851fff0dfddd74cfbacbec9c301eefdd9fde
Arley Souto Aguiar <macx15@hotmail.com>
2020-06-08 17:13:00 -0300

    Teste para js
    teste

--- js/estilo.js
+++ js/estilo.js
@@ -2,3 +2,5 @@ let btnCard = document.getElementById('btnCard')
 let nomeUsuario = document.getElementById('nomeUsuario')
 let setorUsuario = document.getElementById('setorUsuario')
 let Card = document.getElementById('cadastrarCard')
+
+//teste
